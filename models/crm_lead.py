6fb2c928e5357a68e4296a900420218697dead78
sapel <santiago.apel@eynes.com.ar>
2023-07-21 19:26:04 -0300

    [IMP] create method for web form: autocomplete name

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -3,7 +3,19 @@ from odoo import fields, models
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
+    name = fields.Char(required=False)
+    def create(self,vals):
+        res = super(CrmLead,self).create(vals)
+        for rec in res:
+            if rec.name == 'custom_dev':
+                name = res.contact_name or ''
+                for tag in rec.intrest_tag_ids:
+                    name += " | " + tag.name
+                rec.name = name
+        return res
     intrest_tag_ids = fields.Many2many(comodel_name='intrest.tag')
+
+
 
 
 class IntrestTag(models.Model):
