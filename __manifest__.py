19e8ab400ae540e6b9cacd8667e104a0d33ad3c5
sapel <santiago.apel@eynes.com.ar>
2023-07-21 15:07:40 -0300

    Primer commit en la rama main

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'roc_custom',
+    'version': '1.0',
+    'category': 'Custom',
+    'sequence': 15,
+    'summary': 'Odoo v15 module with custom features for roc project',
+    'description': "",
+    'website': '',
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_lead.xml',
+        'data/intrest_tag.xml',
+        'security/ir.model.access.csv',
+    ],
+   
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ models/crm_lead.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    intrest_tag_ids = fields.Many2many(comodel_name='intrest.tag')
+
+
+class IntrestTag(models.Model):
+    _name = 'intrest.tag'
+
+    name = fields.Char
