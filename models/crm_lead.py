44a870ae68472913f9040a2aa7a23601013a1453
sapel <santiago.apel@eynes.com.ar>
2023-07-30 22:53:29 -0300

    [MOD] coment code

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -6,6 +6,7 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
     _order = 'create_date desc'
 
+
     work_type_id = fields.Many2one('crm.work.type', tracking=True)
     source_id = fields.Many2one('utm.source', tracking=True)
     priority = fields.Selection(tracking=True)
@@ -49,48 +50,48 @@ class CrmLead(models.Model):
 
         return partner
 
-    def extract_data_form_html(self, message):
-        html_body = message[0].body
-        html_data = {}
-        try:
-            root = etree.fromstring(html_body, parser=etree.HTMLParser())
-            table = root.xpath("//table[@style='border:0; margin:10px 0']")
-            if table:
-                rows = table[0].xpath(".//tr")
-                for row in rows:
-                    cells = row.xpath(".//td")
-                    if len(cells) == 2:
-                        label = cells[0].text.strip()
-                        value = cells[1].text.strip()
-                        html_data[label] = value
-        except Exception as e:
-            print("Error:", e)
-
-        vals_to_write = {}
-        mail_from = message[0].email_from
-        #if mail_from == 'noreply@secretariado-online.com':
-        #    False
-        if mail_from == 'noreply@chatwith.io':
-            vals_to_write = {
-                'name': 'Llamada de ' + html_data['Nombre'] if html_data['Nombre'] else 'Se ha',
-                'contact_name': html_data['Nombre'],
-                'mobile': html_data['Número WhatsApp'],
-                'zip': html_data['Código postal'],
-                'email_from': html_data['Email'],
-            }
-        return vals_to_write
-    @api.depends('create_date')
-    def extract_data(self):
-        for record in self:
-            if not record.contact_name and not record.partner_id:
-                create_message = record.message_ids.filtered(lambda x: x.email_from in ('noreply@chatwith.io','noreply@secretariado-online.com'))
-                if create_message:
-                        vals_to_write = record.extract_data_form_html(create_message)
-                        if vals_to_write:
-                            record.write(vals_to_write)
-            record.trigger_extract_data = False if record.trigger_extract_data else True
-
-    trigger_extract_data = fields.Boolean(compute=extract_data, store=True)
+    #def extract_data_form_html(self, message):
+    #    html_body = message[0].body
+    #    html_data = {}
+    #    try:
+    #        root = etree.fromstring(html_body, parser=etree.HTMLParser())
+    #        table = root.xpath("//table[@style='border:0; margin:10px 0']")
+    #        if table:
+    #            rows = table[0].xpath(".//tr")
+    #            for row in rows:
+    #                cells = row.xpath(".//td")
+    #                if len(cells) == 2:
+    #                    label = cells[0].text.strip()
+    #                    value = cells[1].text.strip()
+    #                    html_data[label] = value
+    #    except Exception as e:
+    #        print("Error:", e)
+#
+    #    vals_to_write = {}
+    #    mail_from = message[0].email_from
+    #    #if mail_from == 'noreply@secretariado-online.com':
+    #    #    False
+    #    if mail_from == 'noreply@chatwith.io':
+    #        vals_to_write = {
+    #            'name': 'Llamada de ' + html_data['Nombre'] if html_data['Nombre'] else 'Se ha',
+    #            'contact_name': html_data['Nombre'],
+    #            'mobile': html_data['Número WhatsApp'],
+    #            'zip': html_data['Código postal'],
+    #            'email_from': html_data['Email'],
+    #        }
+    #    return vals_to_write
+    #@api.depends('create_date')
+    #def extract_data(self):
+    #    for record in self:
+    #        if not record.contact_name and not record.partner_id:
+    #            create_message = record.message_ids.filtered(lambda x: x.email_from in ('noreply@chatwith.io','noreply@secretariado-online.com'))
+    #            if create_message:
+    #                    vals_to_write = record.extract_data_form_html(create_message)
+    #                    if vals_to_write:
+    #                        record.write(vals_to_write)
+    #        record.trigger_extract_data = False if record.trigger_extract_data else True
+#
+    #trigger_extract_data = fields.Boolean(compute=extract_data, store=True)
     @api.depends('name')
     def compute_name(self):
         for record in self:
