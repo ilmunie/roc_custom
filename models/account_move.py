3abd49dcc8fd42c61959b2dbcbc66212daf51cac
sapel <santiago.apel@eynes.com.ar>
2023-09-05 16:33:19 -0300

    [FIX] inherit function account_move

--- models/account_move.py
+++ models/account_move.py
@@ -7,14 +7,13 @@ class AccountJournal(models.Model):
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def _recompute_dynamic_lines(self):
-        res = super(AccountMove,self)._recompute_dynamic_lines()
+    def _recompute_dynamic_lines(self, recompute_all_taxes=False, recompute_tax_base_amount=False):
+        res = super(AccountMove,self)._recompute_dynamic_lines(recompute_all_taxes=recompute_all_taxes,recompute_tax_base_amount=recompute_tax_base_amount)
         for invoice in self:
             if invoice.journal_id and invoice.journal_id.default_payable_account_id:
                 receivable_account_id = invoice.journal_id.default_payable_account_id.id
                 for line in invoice.line_ids.filtered(lambda x: x.account_id.user_type_id.name in ('receivable','Por cobrar')):
                     line.account_id = receivable_account_id
-
         return res
     @api.onchange('journal_id')
     def _onchange_journal_recompute(self):
