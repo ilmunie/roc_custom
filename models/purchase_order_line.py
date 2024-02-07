054de51aa9d38265684093a2450983c2571ef789
sapel <santiago.apel@eynes.com.ar>
2024-02-07 15:07:16 -0300

    [fix] COMPUTE QTY TO BILL

--- models/purchase_order_line.py
+++ models/purchase_order_line.py
@@ -8,11 +8,25 @@ class PurchaseOrderLine(models.Model):
 
 
 
-    @api.depends('invoice_lines.move_id.state', 'invoice_lines.quantity', 'qty_received', 'product_uom_qty', 'order_id.state', 'trigger_compute_qty_inv')
+    @api.depends('invoice_lines.move_id.state', 'invoice_lines.quantity', 'qty_received', 'product_uom_qty', 'order_id.state', 'trigger_compute_qty_inv', 'product_id.invoice_policy')
     def _compute_qty_invoiced(self):
         for line in self:
             # compute qty_invoiced
-            line.qty_to_invoice = line.product_qty - line.qty_invoiced
+            qty = 0.0
+            for inv_line in line._get_invoice_lines():
+                if inv_line.move_id.state not in ['cancel'] or inv_line.move_id.payment_state == 'invoicing_legacy':
+                    if inv_line.move_id.move_type == 'in_invoice':
+                        qty += inv_line.product_uom_id._compute_quantity(inv_line.quantity, line.product_uom)
+                    elif inv_line.move_id.move_type == 'in_refund':
+                        qty -= inv_line.product_uom_id._compute_quantity(inv_line.quantity, line.product_uom)
+            line.qty_invoiced = qty
+            if line.order_id.state in ['purchase', 'done']:
+                if line.product_id.invoice_policy == 'order':
+                    line.qty_to_invoice = line.product_qty - line.qty_invoiced
+                else:
+                    line.qty_to_invoice = line.qty_received - line.qty_invoiced
+            else:
+                line.qty_to_invoice = 0
 
     trigger_compute_qty_inv = fields.Boolean()
 
