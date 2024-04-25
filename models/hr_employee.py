9f31e5be41d73001807e10a467647414833d44cb
sapel <santiago.apel@eynes.com.ar>
2024-04-25 09:33:08 -0300

    []

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -4,3 +4,8 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     technical = fields.Boolean()
+
+class HrEmployePublic(models.Model):
+    _inherit = 'hr.employee.public'
+
+    technical = fields.Boolean()
