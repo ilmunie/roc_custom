6af1ee644d479b6f3be5802bb6006fba086b969f
sapel <santiago.apel@eynes.com.ar>
2024-06-10 16:46:28 -0300

    [IMP] pos order references

--- models/pos_order.py
+++ models/pos_order.py
@@ -31,44 +31,8 @@ class PosOrder(models.Model):
     @api.depends('state')
     def create_opportunity(self):
         for record in self:
-            if record.sale_order_count == 0 and record.state in ('paid', 'done', 'invoiced') and not record.opportunity_id:
-                user = False
-                if record.employee_id.user_id:
-                    user = record.employee_id.user_id.id
-                else:
-                    users = self.env['res.users'].search([('login','=',record.employee_id.work_email)])
-                    if users:
-                        user = users[0].id
-                medium = False
-                mediums = self.env['utm.medium'].search([('name', '=', 'TIENDA')])
-                if mediums:
-                    medium = mediums[0].id
-                source = False
-                sources = self.env['utm.source'].search([('name', '=', 'tienda')])
-                if sources:
-                    source = sources[0].id
-                partner = False
-                if record.partner_id:
-                    partner = record.partner_id.id
-                else:
-                    generic_partnerts = self.env['res.partner'].search([('name','=','CLIENTE GENERICO')])
-                    if generic_partnerts:
-                        partner = generic_partnerts[0].id
-
-                opportunity_vals = {
-                    'name': record.name,
-                    'type': 'opportunity',
-                    'partner_id': partner,
-                    'stage_id': self.env.ref('crm.stage_lead4').id,
-                    'expected_revenue': record.amount_total - record.amount_tax,
-                    'user_id': user,
-                    'medium_id': medium,
-                    'source_id': source,
-                    'team_id': self.env.ref('sales_team.pos_sales_team').id,
-                }
-                opp = self.env['crm.lead'].create(opportunity_vals)
-                record.opportunity_id = opp.id
             record.trigger_create_opportunity = False if record.trigger_create_opportunity else True
+
     trigger_create_opportunity = fields.Boolean(compute=create_opportunity, store=True)
 
 
