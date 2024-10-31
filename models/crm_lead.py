381f69e0de8ed988a5fc3c60c351f6d23bdf0b69
sapel <santiago.apel@eynes.com.ar>
2024-10-31 10:14:30 -0300

    [ADD] NOTAS LEAD CHAR

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -6,6 +6,9 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
     _order = 'datetime_in_stage,datetime_in_lead_stage desc'
 
+
+    description_char = fields.Char(string="Notas (widget)")
+
     def edit_description(self):
         self.ensure_one()
         action = {
